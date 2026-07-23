ab2c9a6885a146eb6b73211677f8c04fc44acd56
BrRoman <nicolas.bancillon@gmail.com>
2026-07-23 16:07:38 +0200

    Pages, main blocks.

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import Menu from "../components/Menu";
+
+export default function About() {
+  return (
+    <>
+      <Menu />
+      <h1>About page!</h1>
+    </>
+  );
+}
---
+++ src/pages/Products.jsx
@@ -0,0 +1,10 @@
+import Menu from "../components/Menu";
+
+export default function Products() {
+  return (
+    <>
+      <Menu />
+      <h1>Products page!</h1>
+    </>
+  );
+}

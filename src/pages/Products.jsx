a2c3106adca9b4467cb357379847965588969a2c
BrRoman <nicolas.bancillon@gmail.com>
2026-09-16 18:07:32 +0200

    Filtering by category.

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import Menu from "../components/Menu";
 
-// TODO: Filtering and sorting.
-
 export default function Products() {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState();
   const [error, setError] = useState("");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const response = await fetch(
-          "https://nico3113.pythonanywhere.com/api/products/",
+          "https://nico3113.pythonanywhere.com/api/products/" +
+            (filter !== "all" ? "?filter=" + filter : ""),
         );
         if (!response.ok) {
           throw new Error("Server response was not ok");
@@ -27,7 +28,7 @@ export default function Products() {
     };
 
     fetchData();
-  }, []);
+  }, [filter]);
 
   const Product = ({ product }) => {
     return (
@@ -59,14 +60,31 @@ export default function Products() {
     <>
       <Menu />
 
+      <h1>Productos</h1>
+
       {loading && <h1>Loading...</h1>}
 
       {error && <h1>{error}</h1>}
 
       {data.length > 0 && (
-        <div id="products" className="grid grid-cols-1 md:grid-cols-4 gap-4">
-          {displayProducts}
-        </div>
+        <>
+          <div className="flex flex-col md:flex-row md:items-center mx-4 mb-4">
+            <p className="text-left text-white p-2">Filtrar por categoría:</p>
+            <select
+              className="bg-white text-gray-700 p-2 rounded shadow md:w-1/4"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            >
+              <option value="all"> Todo</option>
+              <option value="rosario">Rosarios</option>
+              <option value="decenario">Decenarios</option>
+              <option value="jabon">Jabones</option>
+            </select>
+          </div>
+          <div id="products" className="grid grid-cols-1 md:grid-cols-4 gap-4">
+            {displayProducts}
+          </div>
+        </>
       )}
     </>
   );
